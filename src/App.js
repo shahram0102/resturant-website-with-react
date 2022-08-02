b0d5b86440798d64b638ece828cc35b7c4a7150b
shahram <shahramkhaleh2001@gmail.com>
2022-08-02 00:31:51 -0700

    created header Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+
+// components
+import { Header } from "./components/Header";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div>
+      <Header />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+
+// react-router-dom
+import { Link } from "react-router-dom";
+
+// images
+import logo from "../images/logowebsite.png";
+import avatar from "../images/User-Avatar.png";
+
+// icons
+import { MdOutlineSearch } from "react-icons/md";
+import { MdOutlineShoppingCart } from "react-icons/md";
+import { RiMenu4Fill } from "react-icons/ri";
+
+export const Header = () => {
+  const [isShowName, setIsShowName] = useState(false);
+  return (
+    <header className="sticky shadow-sm shadow-orange-300 ">
+      <div className="container m-auto justify-between  flex px-4 py-2">
+        <div className="flex gap-1 md:w-5/6 w-4/6">
+          <Link to="/">
+            <div className="flex justify-center w-10 h-10 rounded-full overflow-hidden items-center">
+              <img
+                src={logo}
+                alt="logo-website"
+                className="w-full object-cover  h-full"
+              />
+            </div>
+          </Link>
+          <label className="flex w-44  md:w-full px-2 gap-2 justify-center bg-zinc-900  items-center shadow-md rounded-full">
+            <MdOutlineSearch className="text-xl" />
+            <input
+              type={"text"}
+              placeholder="search"
+              className="bg-transparent flex-1 w-full placeholder:text-orange-300 outline-none border-none"
+            />
+          </label>
+        </div>
+        <div className="flex w-2/6 md:w-1/6 justify-end">
+          <div className="flex relative cursor-pointer justify-center pr-2 pt-1 items-center">
+            <MdOutlineShoppingCart className="text-orange-300 text-3xl" />
+            <span className="absolute top-0 right-0 bg-orange-500 text-white w-5 h-5 rounded-full flex justify-center items-center">
+              2
+            </span>
+          </div>
+          <div className="flex justify-center w-20 items-center gap-1">
+            {!isShowName && (
+              <div className="w-10 h-10">
+                <img className="w-full h-full object-cover" src={avatar} />
+              </div>
+            )}
+            {isShowName && <h2>shahram</h2>}
+          </div>
+
+          <div
+            onClick={() => setIsShowName(!isShowName)}
+            className="flex justify-center items-center"
+          >
+            <RiMenu4Fill className="text-2xl cursor-pointer" />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
